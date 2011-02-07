a7822f8e1eda58844baa71eb563e15fa116ba912
Stevan Little <stevan.little@iinteractive.com>
2011-02-07 16:13:09 -0500

    the 3xx codes

---
+++ lib/HTTP/Throwable/Found.pm
@@ -0,0 +1,61 @@
+package HTTP::Throwable::Found;
+use Moose;
+
+extends 'HTTP::Throwable';
+
+has '+status_code' => ( default => 302 );
+has '+reason'      => ( default => 'Found' );
+
+has 'temp_location' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+has 'cache_headers' => ( is => 'ro', isa => 'ArrayRef' );
+
+around 'build_headers' => sub {
+    my $next    = shift;
+    my $self    = shift;
+    my $headers = $self->$next( @_ );
+    push @$headers => ('Location' => $self->temp_location);
+    if ( my $cache_headers = $self->cache_headers ) {
+        push @$headers => @$cache_headers;
+    }
+    $headers;
+};
+
+__PACKAGE__->meta->make_immutable;
+
+no Moose; 1;
+
+__END__
+
+# ABSTRACT: 302 Found
+
+=head1 DESCRIPTION
+
+The requested resource resides temporarily under a different URI.
+Since the redirection might be altered on occasion, the client
+SHOULD continue to use the Request-URI for future requests. This
+response is only cacheable if indicated by a Cache-Control or
+Expires header field.
+
+The temporary URI SHOULD be given by the Location field in the
+response. Unless the request method was HEAD, the entity of the
+response SHOULD contain a short hypertext note with a hyperlink
+to the new URI(s).
+
+=head1 ATTRIBUTES
+
+=attr temp_location
+
+This is a required string, which will be used in the Location header
+when creating a PSGI response.
+
+=attr cache_headers
+
+This is an optional ArrayRef containing HTTP headers that will be
+included when creating the PSGI response.
+
+
---
+++ lib/HTTP/Throwable/MultipleChoices.pm
@@ -0,0 +1,61 @@
+package HTTP::Throwable::MultipleChoices;
+use Moose;
+
+extends 'HTTP::Throwable';
+
+has '+status_code' => ( default => 300 );
+has '+reason'      => ( default => 'Multiple Choices' );
+
+has 'preferred_location' => ( is => 'ro', isa => 'Str' );
+
+around 'build_headers' => sub {
+    my $next    = shift;
+    my $self    = shift;
+    my $headers = $self->$next( @_ );
+    if ( my $preferred_location = $self->preferred_location ) {
+        push @$headers => ('Location' => $preferred_location);
+    }
+    $headers;
+};
+
+__PACKAGE__->meta->make_immutable;
+
+no Moose; 1;
+
+__END__
+
+# ABSTRACT: 300 Multiple Choices
+
+=head1 DESCRIPTION
+
+The requested resource corresponds to any one of a set of representations,
+each with its own specific location, and agent-driven negotiation information
+is being provided so that the user (or user agent) can select a preferred
+representation and redirect its request to that location.
+
+Unless it was a HEAD request, the response SHOULD include an entity containing
+a list of resource characteristics and location(s) from which the user or user
+agent can choose the one most appropriate. The entity format is specified by
+the media type given in the Content-Type header field. Depending upon the
+format and the capabilities of the user agent, selection of the most appropriate
+choice MAY be performed automatically. However, this specification does not
+define any standard for such automatic selection.
+
+If the server has a preferred choice of representation, it SHOULD include
+the specific URI for that representation in the Location field; user agents
+MAY use the Location field value for automatic redirection. This response is
+cacheable unless indicated otherwise.
+
+=head1 ATTRIBUTES
+
+=attr preferred_location
+
+This is an optional string, which, if supplied, will be used in the Location
+header when creating a PSGI response.
+
+
+
+
+
+
+
