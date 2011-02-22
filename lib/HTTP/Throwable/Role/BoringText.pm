330c012a584bff374a870bdfa05ad7d79bf18ca6
Ricardo Signes <rjbs@cpan.org>
2011-02-21 20:43:44 -0500

    document the BoringText role

--- lib/HTTP/Throwable/Role/BoringText.pm
+++ lib/HTTP/Throwable/Role/BoringText.pm
@@ -7,3 +7,17 @@ sub text_body { $_[0]->status_line }
 
 no Moose::Role;
 1;
+
+__END__
+# ABSTRACT: provide the simplest text_body method possible
+
+=head1 OVERVIEW
+
+This role is as simple as can be.  It provides a single method, C<text_body>,
+which returns the result of calling the C<status_line> method.
+
+This method exists so that exception classes can easily be compatible with the
+L<HTTP::Throwable::Role::TextBody> role to provide a plain text body when
+converted to an HTTP message.  Most of the core well-known exception types
+consume this method.
+
