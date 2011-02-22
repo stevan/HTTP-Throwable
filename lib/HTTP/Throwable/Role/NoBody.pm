777c65f8e550ea21ac1f1947499727361c816943
Ricardo Signes <rjbs@cpan.org>
2011-02-21 19:07:50 -0500

    document the simple body-providing roles

--- lib/HTTP/Throwable/Role/NoBody.pm
+++ lib/HTTP/Throwable/Role/NoBody.pm
@@ -15,3 +15,15 @@ sub as_string { $_[0]->status_line }
 
 no Moose::Role;
 1;
+
+__END__
+# ABSTRACT: an exception with no body
+
+=head1 OVERVIEW
+
+This is a very simple role, implementing the required C<as_string>, C<body>,
+and C<body_headers> for L<HTTP::Throwable>.
+
+When an HTTP::Throwable exception uses this role, its PSGI response will have
+no entity body.  It will report a Content-Length of zero.  It will stringify to
+its status line.
--- lib/HTTP/Throwable/Role/TextBody.pm
+++ lib/HTTP/Throwable/Role/TextBody.pm
@@ -14,5 +14,23 @@ sub body_headers {
 
 sub as_string { $_[0]->body }
 
+requires 'text_body';
+
 no Moose::Role;
 1;
+
+__END__
+# ABSTRACT: an exception with a plaintext body
+
+=head1 OVERVIEW
+
+This is a very simple role, implementing the required C<as_string>, C<body>,
+and C<body_headers> for L<HTTP::Throwable>.  In turn, it requires that a
+C<text_body> method be provided.
+
+When an HTTP::Throwable exception uses this role, its PSGI response will have a
+C<text/plain> content type and will send the result of calling its C<text_body>
+method as the response body.  It will also stringify to the text body.
+
+The role L<HTTP::Throwable::Role::BoringText> can be useful to provide a
+C<text_body> method that issues the C<status_line> as the body.
