3d0b991872a4508d07c84310b654d815a113f212
Ricardo Signes <rjbs@cpan.org>
2011-02-21 20:43:33 -0500

    document the redirect role

--- lib/HTTP/Throwable/Role/Redirect.pm
+++ lib/HTTP/Throwable/Role/Redirect.pm
@@ -17,3 +17,16 @@ around 'build_headers' => sub {
 };
 
 no Moose::Role; 1;
+
+__END__
+# ABSTRACT: an exception that is a redirect
+
+=head1 OVERVIEW
+
+This is an extremely simple method used by most of the 3xx series of
+exceptions.  It adds a C<location> attribute that will be provided as the
+Location header when the exception is converted to an HTTP message.
+
+Note that "MultipleChoices," the 300 status code is I<not> currently a
+redirect, as its Location header is optional.  This may change in the future of
+the semantics of this role are refined.
